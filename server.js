8de365cdd2723fa18c74a49e4775f2cc8b221252
Pedro Affonso Silva Marques <89036165+pasmarques@users.noreply.github.com>
2024-11-03 15:32:32 -0300

    padronizando respostas para pt-br

--- server.js
+++ server.js
@@ -8,31 +8,32 @@ app.use(express.json())
 
 // URL PARA O POSTMAN (POST e GET): http://localhost:3000/students
 
-app.post("/students", (req,res)=>{
-  const newStudent = req.body
+app.post("/students", (req, res) => {
+  const newStudent = req.body;
   
-  if ((!newStudent.name) || (!newStudent.email) || (!newStudent.course_name)){
-    return res.status(400).json({message: "All fields are mandatory: name,email and course_name"});
+  if (!newStudent.name || !newStudent.email || !newStudent.course_name) {
+    return res.status(400).json({ message: "Todos os campos são obrigatórios: name, email e course_name." });
   }
-  create(newStudent)
-  return res.status(201).json({message: `Student ${newStudent.name} created sucessfully`});
+  create(newStudent);
+  return res.status(201).json({ message: `Estudante ${newStudent.name} criado com sucesso.` });
 });
 
 
-app.get('/students', (req,res)=>{
+app.get('/students', (req, res) => {
   const students = findAll();
 
   if (!students) {
-    return res.status(404).json({ error: "No students found" });
+    return res.status(404).json({ message: "Nenhum estudante encontrado." });
   }
   res.status(200).json(students);
 });
 
-app.get('/students/:id', (req,res)=>{
-  const { id } =  req.params;
+app.get('/students/:id', (req, res) => {
+  const { id } = req.params;
   const student = findById(id);
+  
   if (!student) {
-    return res.status(404).json({ error: `Nenhum estudante encontrado para o id: ${id}` });
+    return res.status(404).json({ message: `Nenhum estudante encontrado para o id: ${id}.` });
   }  
   res.status(200).json(student);
 });
@@ -42,9 +43,9 @@ app.delete('/students/:id', (req, res) => {
   const deletedStudent = deleteById(id);
 
   if (deletedStudent) {
-    return res.status(200).json({ message: `Student with id ${id} deleted successfully` });
+    return res.status(200).json({ message: `Estudante com o id ${id} excluído com sucesso.` });
   } else {
-    return res.status(404).json({ message: `Student with id ${id} not found` });
+    return res.status(404).json({ message: `Estudante com o id ${id} não encontrado.` });
   }
 });
 
@@ -59,13 +60,12 @@ app.put('/students/:id', (req, res) => {
   const updatedStudent = update(id, updatedData);
 
   if (updatedStudent) {
-    return res.status(200).json({ message: `O estudante com o ${id} foi atualizado com sucesso.`, updatedStudent });
+    return res.status(200).json({ message: `O estudante com o id ${id} foi atualizado com sucesso.`, updatedStudent });
   } else {
     return res.status(404).json({ message: `O estudante com o id ${id} não foi encontrado.` });
   }
 });
 
-  
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
